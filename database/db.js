2db5ef3fa05c70775bb4af2d5df265e997ad791a
Irfannur Diah <kang.fand@gmail.com>
2022-02-28 02:40:42 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.reysekha.xyz';
+var url = 'https://rest-api1-production-c979.up.railway.app/';
 try {
-if(url == 'https://api.reysekha.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://rest-api1-production-c979.up.railway.app/') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
